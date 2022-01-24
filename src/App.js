52da94e42056dd6bfb856616cbf448731e9587b3
hamsaana <hamsaanaselvan@gmail.com>
2022-01-24 18:02:40 -0500

    adding API

--- src/App.js
+++ src/App.js
@@ -1,38 +1,103 @@
-import Randomizer from './Component/Randomizer.js';
-import {useEffect} from 'react';
-import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
 
-  
+  useEffect(() => {
+    axios({
+      url: 'https://api.datamuse.com/sug?',
+      method: 'GET',
+      dataType: 'json',
+      params: {
+          s: "m",
+          max: 10
+      }
 
+
+    }).then((response) => {
+      console.log(response.data);
+    })
+  })
 
   return (
     <div>
       <h1>TESSST!</h1>
-      <Randomizer />
+      <div>
+
+        <div>
+        <form onSubmit={handleSubmitOne}>
+          <label htmlFor="one"></label>
+          <input type="text" id="one" maxLength={1} placeholder="One" onChange={handleInputOne} value={inputOne} />
+          <button>ADD</button>
+        </form>
+
+        {
+          infoOne.map((results) => {
+            return (
+              <li>
+                <p>{results['word']} </p>
+              </li>
+            )
+          })
+        }
+        </div>
+
+        <div>
+        <form onSubmit={handleSubmitTwo}>
+          <label htmlFor="two"></label>
+            <input type="text" id="two" maxLength={1} placeholder="Two" onChange={handleInputTwo} value={inputTwo} />
+          <button>ADD</button>
+        </form>
+
+          {
+            infoTwo.map((resultss) => {
+              return (
+                <li>
+                  <p>{resultss['word']} </p>
+                </li>
+              )
+            })
+          }
+        </div>
+
+        <div>
+        <form onSubmit={handleSubmitThree}>
+          <label htmlFor="three"></label>
+            <input type="text" id="three" maxLength={1} placeholder="Three" onChange={handleInputThree} value={inputThree} />
+          <button>ADD</button>
+        </form>
+
+          {
+            infoThree.map((resultsss) => {
+              return (
+                <li>
+                  <p>{resultsss['word']} </p>
+                </li>
+              )
+            })
+          }
+        </div>
+
+        <div>
+        <form onSubmit={handleSubmitFour}>
+          <label htmlFor="four"></label>
+            <input type="text" id="four" maxLength={1} placeholder="Four" onChange={handleInputFour} value={inputFour}/>
+          <button>ADD</button>
+        </form>
+
+          {
+            infoFour.map((resultssss) => {
+              return (
+                <li>
+                  <p>{resultssss['word']} </p>
+                </li>
+              )
+            })
+          }
+        </div >
+      </div>
     </div>
   );
 }
 
 export default App;
-
-// Create 4-5 input for user to input individual letters of their chosen acronym 
-// Take the user's input and post words that start with each letter of user's input
-// Create firebase database to save user's selection
-// Display user's selection in a different route
-
-
-// STEPS
-// Get info from api using axios or fetch
-// Create multiple calls per user's letter input
-    // Create form per letter (max 5) 
-    // Call api on submit 
-    // Save data to local variable 
-    // Randomize data 
-    // Save return to useState
-// Save user's choice of backcronym in total
-    // 
-// Save chosen backronym to firebase database and display to page
-// 
-
