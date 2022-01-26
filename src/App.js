af32db56f9efcb8a4874ddd9fa30fcdfef56e00e
Andre <andre.shotwell@gmail.com>
2022-01-26 17:05:22 -0500

    updated our api call

--- src/App.js
+++ src/App.js
@@ -2,134 +2,90 @@ import axios from 'axios';
 import './App.css';
 import { useEffect, useState } from 'react';
 
+import Header from './components/Header';
+// import Form from './components/Form';
+import Footer from './components/Footer';
+
+
 function App() {
-  // const [ inputOne, setInputOne ] = useState("");
-  // const [ inputTwo, setInputTwo ] = useState("");
-  // const [ inputThree, setInputThree ] = useState("");
-  // const [ inputFour, setInputFour ] = useState("");
-  const inputOne = 't';
-  const inputTwo = 'g';
-  const inputThree = 'i';
-  const inputFour = 'f';
 
-  const userInputs = [ inputOne, inputTwo, inputThree, inputFour ];
+  const [ inputOne, setInputOne ] = useState("");
+  const [ inputTwo, setInputTwo ] = useState("");
+  const [ inputThree, setInputThree ] = useState("");
+  const [ inputFour, setInputFour ] = useState("");
 
-  const [ randomWord, setRandomWord] = useState("");
+  const userInputs = [];
 
   const backronymArray = [];
 
-  const callDataMuse = userInputs.map((userInput) => {
-    axios({
-        url: "https://api.datamuse.com/sug?",
-        method: 'GET',
-        dataType: 'json',
-        params: {
+  function callDataMuse(userInput) {
+    return axios({
+      url: "https://api.datamuse.com/sug?",
+      method: 'GET',
+      dataType: 'json',
+      params: {
         s: userInput,
         max: 20, 
-        },
-
-      }).then((response) => {
-        
-      const resultsArray = response.data;
-      // console.log(resultsArray, 'dataresponse works');
-      const randomIndex = Math.floor(Math.random() * resultsArray.length);
-      console.log(randomIndex, 'index works');
-      // const randomWordLocal = resultsArray[randomIndex].word;
-      // console.log(randomWordLocal, 'last one works');
-      setRandomWord(resultsArray[randomIndex].word);
-      console.log(resultsArray[randomIndex].word);
-      })
-
+      },
+    
+    }).then( (response) => {
+      console.log(response.data)
+      return response;
     });
+  }
 
-    console.log(callDataMuse);
+  for(let i = 1; i <= 4; i++) {
+    userInputs.push(callDataMuse(i));
+    // console.log(userInputs)
+  }
 
+  Promise.all(userInputs)
+    .then(responses => {
+      console.log(responses)
+  })
+  
 
-    Promise.all(callDataMuse)
-      .then((randomWord) => {
-        console.log(randomWord)
-      })
-
-    // console.log(callDataMuse, 'this is working');
-
-
-
-
-    // useEffect(() => {
-    //   // callDataMuse();
-    // }, []);
-
-    return (
+      return (
         <div className="App">
             <h1> check console </h1>
         </div>
     );
-}
+  }
+
 
 export default App;
 
 
-// WE WERE ABLE TO GET RANDOM WORD BACK FOR A SINGLE INPUT
 
-// function App() {
-  // const [ inputOne, setInputOne ] = useState("");
-  // const [ inputTwo, setInputTwo ] = useState("");
-  // const [ inputThree, setInputThree ] = useState("");
-  // const [ inputFour, setInputFour ] = useState("");
-//   const inputOne = 't';
-//   const inputTwo = 'g';
-//   const inputThree = 'i';
-//   const inputFour = 'f';
 
-//   const userInputs = [ inputOne, inputTwo, inputThree, inputFour ];
+//   // const userInputs = [ inputOne, inputTwo, inputThree, inputFour ];
 //   // console.log(userInputs);
 
-//   const [ randomWord, setRandomWord] = useState([]);
+//   // const [ randomWord, setRandomWord] = useState([]);
 
-//   const backronymArray = [];
+//   // const callDataMuse = userInputs.map((userInput) => {
+//   //   const backronymArray = [];
+//   //   axios({
+//   //       url: "https://api.datamuse.com/sug?",
+//   //       method: 'GET',
+//   //       dataType: 'json',
+//   //       params: {
+//   //         s: userInput,
+//   //         max: 20, 
+//   //       },
 
-//   const callDataMuse = (() => {
-//     axios({
-//         url: "https://api.datamuse.com/sug?",
-//         method: 'GET',
-//         dataType: 'json',
-//         params: {
-//           s: inputOne,
-//           max: 20, 
-//           },
+//   //     }).then((response) => {
 
-//       }).then((response) => {
+//   //       return response;
         
-//       const resultsArray = response.data;
-//       // console.log(resultsArray, 'dataresponse works');
-//       const randomIndex = Math.floor(Math.random() * resultsArray.length);
-//       // console.log(randomIndex, 'index works');
-//       // const randomWordLocal = resultsArray[randomIndex].word;
-//       // console.log(randomWordLocal, 'last one works');
-//       setRandomWord(resultsArray[randomIndex].word);
-      
-//       })
-//     });
+//   //     // const resultsArray = response.data;
+//   //     // // console.log(resultsArray, 'dataresponse works');
+//   //     // const randomIndex = Math.floor(Math.random() * resultsArray.length);
+//   //     // // console.log(randomIndex, 'index works');
+//   //     // setRandomWord(resultsArray[randomIndex].word);
+//   //     // // console.log(randomWordLocal, 'last one works');
+//   //     // console.log(resultsArray[randomIndex].word);
+//   //     })
+//   //   });
+
 //     // console.log(callDataMuse);
-
-//     // console.log(alert);
-
-//     // Promise.all(callDataMuse)
-//     //   .then((randomWord) => {
-//         console.log(randomWord)
-//     //   })
-
-//     // console.log(callDataMuse, 'this is working');
-
-
-
-//     useEffect(() => {
-//       callDataMuse();
-//     }, []);
-
-//     return (
-//         <div className="App">
-//             <h1> check console </h1>
-//         </div>
-//     );
-// }
--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,9 @@ import { useEffect } from "react/cjs/react.development";
 // Form component
 const Form = () => {
 
+    
+
+
     // const initialValues =
     //     letterOne: "",
     //     letterTwo: "",
